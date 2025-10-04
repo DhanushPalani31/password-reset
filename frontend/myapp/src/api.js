87cfcb37825b0c33aebd35f0942d0088b6d95a6e
DhanushPalani31 <dhanushcse00@gmail.com>
2025-10-04 13:36:38 +0530

    fix:env file

--- frontend/myapp/src/api.js
+++ frontend/myapp/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://localhost:5000/api/auth", 
-});
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
-export default api;
+export default axios.create({
+  baseURL: `${API_URL}/api`,
+});
